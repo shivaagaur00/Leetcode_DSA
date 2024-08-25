0f2aa6fbcbbcd8a3107ef718a180e09c9721ae25
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-08-25 19:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (64.32%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -0,0 +1,10 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int[] arr=new int[26];
+        for(char c:s.toCharArray()){
+            arr[c-'a']++;
+            if(arr[c-'a']==2) return c;
+        }
+        return 'a';
+    }
+}
