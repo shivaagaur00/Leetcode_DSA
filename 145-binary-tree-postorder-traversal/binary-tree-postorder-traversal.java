e09a19c53364cf2df47da7a8c13f4d43da6f2b7a
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-08-25 10:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (15.01%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -1,18 +1,28 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
-        if (root == null) return arr;
-        Stack<TreeNode> stack = new Stack<>();
-        stack.add(root);
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            arr.add(node.val);
-            if (node.left != null) stack.add(node.left);
-            if (node.right != null) stack.add(node.right);
-        }
-        Collections.reverse(arr);
+        List<Integer> arr=new ArrayList<>();
+        func(root,arr);
         return arr;
+    }
+    public void func(TreeNode root,List<Integer> arr){
+        if(root==null) return;
+        func(root.left,arr);
+        func(root.right,arr);
+        arr.add(root.val);
     }
 }
