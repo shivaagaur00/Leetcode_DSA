af429b615374de1f1741632159065b66e5640562
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-08-29 23:17:36 +0530

    Time: 7 ms (23.50%) | Memory: 45.2 MB (72.20%) - LeetSync

---
+++ 508-most-frequent-subtree-sum/most-frequent-subtree-sum.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer,Integer> map=new HashMap<>();
+    Map<Integer,Integer> tr=new HashMap<>();
+    public int[] findFrequentTreeSum(TreeNode root) {
+        func(root,0);
+        int max=Integer.MIN_VALUE;
+        int element=root.val;
+        for(int key:map.keySet()){
+            max=Math.max(max,map.get(key));
+        }
+        List<Integer> arr=new ArrayList<>();
+        for(int key:map.keySet()){
+            if(map.get(key)==max) arr.add(key);
+        }
+        int[] ans=new int[arr.size()];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=arr.get(i);
+        }
+        return ans;
+
+    }
+    public int func(TreeNode root,int sum){
+        if(root==null) return 0;
+        int a=func(root.left,sum);
+        int b=func(root.right,sum);
+        int total=a+b+root.val;
+        map.put(total,map.getOrDefault(total,0)+1);
+        tr.put(total,root.val);
+        return total;
+    }
+}
