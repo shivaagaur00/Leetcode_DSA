5c9e89628b0f8aabd8ea2064cfa07533e4e4a0c1
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-09-06 08:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (30.28%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -11,20 +11,20 @@
  *         this.left = left;
  *         this.right = right;
  *     }
- * } */
+ * }
+ */
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
-        TreeNode root=new TreeNode(preorder[0]);
-        // Queue<TreeNode> stack=new L<>();
+        TreeNode root=null;
         for(int i=0;i<preorder.length;i++){
             root=func(root,preorder[i]);
-        }        
+        }
         return root;
     }
     public TreeNode func(TreeNode root,int val){
         if(root==null) return new TreeNode(val);
-        if(root.val<val) root.right=func(root.right,val);
         if(root.val>val) root.left=func(root.left,val);
+        else if(root.val<val) root.right=func(root.right,val);
         return root;
     }
 }
