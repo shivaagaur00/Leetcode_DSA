01836bf450367b05eb7284b0d44ca2cda9a86087
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-09-14 19:03:35 +0530

    Time: 4 ms (87.73%) | Memory: 45.1 MB (87.73%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
-        if(s.length()<k) return false;
-        int[] count=new int[26];
-        for(int i:s.toCharArray()){
-            count[i-'a']++;
+        if (s.length() < k)
+            return false;
+        int[] count = new int[26];
+        for (int i : s.toCharArray()) {
+            count[i - 'a']++;
         }
-        int even=0;
-        List<Integer> arr=new ArrayList<>();
-        for(int a:count){
-        if(a%2!=0){
-            arr.add(a);
+        int odd = 0;
+        for (int a : count) {
+            if (a % 2 != 0) {
+                odd++;
+            }
         }
-        else{
-            even+=(a/2);
-        }
-        }
-        if(arr.size()>k) return false;
+        if (odd > k)
+            return false;
         return true;
     }
 }
