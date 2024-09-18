912adc53239fe3189fda4c50bbecf883d0ce8c4a
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-09-18 23:45:37 +0530

    Time: 1 ms (51.06%) | Memory: 57.7 MB (7.89%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+       int temp[]=new int[nums.length];
+       k%=nums.length;
+       for(int i=0;i<nums.length;i++){
+        temp[(i+k)%nums.length]=nums[i];
+       } 
+       for(int i=0;i<nums.length;i++) nums[i]=temp[i];
+    }
+}
