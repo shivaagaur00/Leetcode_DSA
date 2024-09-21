bc36e54b73f1e3604e2663312c165a13f9503c8e
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-09-22 01:22:36 +0530

    Time: 3 ms (79.73%) | Memory: 45.5 MB (58.96%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,28 +1,32 @@
 class Solution {
     public int candy(int[] ratings) {
-        int ans=0;
-        int[] left=new int[ratings.length];
-        int[] right=new int[ratings.length];
-        left[0]=1;
-        int curr=1;
-        for(int i=1;i<ratings.length;i++){
-            if(ratings[i]>ratings[i-1]) left[i]=++curr;
-            else{
-                curr=1;
-                left[i]=1;
+        int ans = 0;
+        int n = ratings.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = 1;
+        int curr = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1])
+                left[i] = ++curr;
+            else {
+                curr = 1;
+                left[i] = 1;
             }
         }
-        curr=1;
-        right[ratings.length-1]=1;
-        for(int i=ratings.length-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]) right[i]=++curr;
-            else{
-                curr=1;
-                right[i]=1;;
+        curr = 1;
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1])
+                right[i] = ++curr;
+            else {
+                curr = 1;
+                right[i] = 1;
+                ;
             }
         }
-        for(int i=0;i<ratings.length;i++){
-            ans+=(Math.max(left[i],right[i]));
+        for (int i = 0; i < n; i++) {
+            ans += (Math.max(left[i], right[i]));
         }
         return ans;
     }
