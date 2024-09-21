1c3fdd65d07edb5dc7931abbba66d21fabfb17b6
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-09-21 23:47:51 +0530

    Time: 2 ms (98.19%) | Memory: 56.3 MB (69.03%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -5,14 +5,14 @@ class Solution {
         int pos=-1;
         for(int i=0;i<gas.length;i++){
             int diff=gas[i]-cost[i];
-            curr+=diff;
             total+=diff;
+            curr+=diff;
             if(curr<0){
                 curr=0;
                 pos=i;
             }
         }
         if(total>=0) return pos+1;
-        return -1;
+        else return -1;
     }
 }
