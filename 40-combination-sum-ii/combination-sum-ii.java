0c4e5d3713b8d6e901f035817f8fb48d61e52c5a
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-09-21 16:47:32 +0530

    Time: 5 ms (19.10%) | Memory: 43.2 MB (39.99%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> arr=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        Arrays.sort(candidates);
+        func(arr,list,0,0,target,candidates);
+        return arr;
+    }
+    public void func(List<List<Integer>> arr,List<Integer> list,int i,int sum,int target,int[] candidates){
+        if(sum==target){
+            if(!arr.contains(list)) arr.add(new ArrayList<>(list));
+            return;
+        }
+        if(sum>target) return;
+        for(int j=i;j<candidates.length;j++){
+            if(j>i && candidates[j]==candidates[j-1]) continue;
+            if(sum>target) break;
+            list.add(candidates[j]);
+            func(arr,list,j+1,sum+candidates[j],target,candidates);
+            list.remove(list.size()-1);
+        }
+        return;
+    }
+}
