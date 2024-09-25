32b9b31c47227a0afa3add1b1769b526a599232e
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-09-25 11:50:24 +0530

    Time: 6 ms (29.11%) | Memory: 44.7 MB (15.25%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -13,11 +13,9 @@ class Solution {
         if(lists.length==0) return null;
         PriorityQueue<Integer> pq=new PriorityQueue<>();
         for(ListNode list:lists){
-            if(list!=null){
             while(list!=null){
                 pq.add(list.val);
                 list=list.next;
-            }
             }
         }
         ListNode ans=new ListNode(0);
