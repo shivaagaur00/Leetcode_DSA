a35716469c3591e8a849c69e7834f2139511242f
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-10-17 18:51:53 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (58.37%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.java
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -1,8 +1,23 @@
 class Solution {
     public int buyChoco(int[] arr, int money) {
      int ans=0;
-     Arrays.sort(arr);
-     if(arr[0]+arr[1]<=money) return money-(arr[0]+arr[1]);
+     int[] one=new int[2];
+     int [] two=new int[2];
+     one[0]=Integer.MAX_VALUE;
+     two[0]=Integer.MAX_VALUE;
+     for(int i=0;i<arr.length;i++) {
+        if(one[0]>arr[i]){
+            one[1]=i;
+            one[0]=arr[i];
+        }
+     }
+     for(int i=0;i<arr.length;i++) {
+        if(i!=one[1] && two[0]>arr[i] ){
+            two[1]=i;
+            two[0]=arr[i];
+        }
+     }
+     if((one[0]+two[0])<=money) return money-(one[0]+two[0]);
      return money;
     }
 }
