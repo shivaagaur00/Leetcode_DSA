ac7e6583f0aae90f85a09ae41f3b76d1f4895d29
shivaagaur00 <115822667+shivaagaur00@users.noreply.github.com>
2024-10-20 12:46:08 +0530

    Time: 4 ms (91.45%) | Memory: 44.5 MB (56.58%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -1,14 +1,9 @@
 class Solution {
     public int numSub(String s) {
-        List<String> arr=new ArrayList<>();
         int temp=0;
         int ans=0;
         for(char c:s.toCharArray()){
-            if(c=='1'){
-                temp++;
-                ans+=temp;
-                ans=ans%1000000007;
-            }
+            if(c=='1') ans=(ans+(++temp))%1000000007;
             else temp=0;
         }
         return ans;
