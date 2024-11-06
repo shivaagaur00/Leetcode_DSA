c94be769e5fced8930d6861612330009ad1d3b59
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2024-11-06 11:44:49 +0530

    Time: 74 ms (5.71%) | Memory: 45.3 MB (5.66%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -1,32 +1,35 @@
 class Solution {
     public boolean canSortArray(int[] nums) {
-        List<List<Integer>> ls=new ArrayList<>();
+        List<pair> ls=new ArrayList<>();
         int bit[]=new int[nums.length];
         for(int i=0;i<nums.length;i++){
             bit[i]=Integer.bitCount(nums[i]);
+            System.out.print(bit[i]+" ");
         }
-        ls.add(new ArrayList<>());
-        ls.get(0).add(nums[0]);
-        System.out.println(ls);
+        ls.add(new pair(nums[0],nums[0]));
         for(int i=1;i<nums.length;i++){
             if(bit[i]==bit[i-1]){
-                ls.get(ls.size()-1).add(nums[i]);
+                pair temp=ls.get(ls.size()-1);
+                temp.max=Math.max(temp.max,nums[i]);
+                temp.min=Math.min(temp.min,nums[i]);
             }
             else{
-                ls.add(new ArrayList<>());
-                ls.get(ls.size()-1).add(nums[i]);
+                ls.add(new pair(nums[i],nums[i]));
             }
         }
-        List<Integer> ans=new ArrayList<>();
-        for(List<Integer> arr:ls){
-            Collections.sort(arr);
-            for(int a:arr){
-                ans.add(a);
-            }
-        }
-        for(int i=1;i<ans.size();i++){
-            if(ans.get(i)<ans.get(i-1)) return false;
+        int min=Math.max(ls.get(0).min,ls.get(0).max);
+        for(int i=1;i<ls.size();i++){
+            if(min>ls.get(i).min || min>ls.get(i).max) return false; 
+            min=Math.max(ls.get(i).max,min);         
         }
         return true;
+    }
+}
+class pair{
+    int max;
+    int min;
+    pair(int min,int max){
+        this.min=min;
+        this.max=max;
     }
 }
