5170a03d67d757a19e16e82914b3211b1f8fc26a
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2024-11-17 16:53:09 +0530

    Time: 5 ms (80.26%) | Memory: 46 MB (11.61%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean canFinish(int n, int[][] arr) {
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(i,new ArrayList<>());
+        }
+        // int[] vis=new boolean[n];
+        // boolean flag=false;
+        for(int i=0;i<arr.length;i++){
+            map.get(arr[i][0]).add(arr[i][1]);
+        }
+        int[] visited = new int[n];
+        boolean hasCycle = false;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                if (dfs(i, map, visited)) {
+                    hasCycle = true;
+                    break;
+                }
+            }
+        }
+
+        if (hasCycle) {
+            return false;
+        } 
+        return true;
+    }
+
+    public boolean dfs(int course, Map<Integer, List<Integer>> graph, int[] visited) {
+        visited[course] = 1; 
+        for (int nextCourse : graph.get(course)) {
+            if (visited[nextCourse] == 1) {
+                return true; 
+            }
+            if (visited[nextCourse] == 0 && dfs(nextCourse, graph, visited)) {
+                return true; 
+            }
+        }
+        visited[course] = 2;
+        return false;
+    }
+}
+class pair{
+    int a;
+    int b;
+    pair(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+}
