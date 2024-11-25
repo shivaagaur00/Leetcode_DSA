d8894b88afb817ce42fac550a9e444c37fb08552
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2024-11-25 08:47:59 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (13.53%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,32 +1,31 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n=obstacleGrid.length;
-        int m=obstacleGrid[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(obstacleGrid[i][j]==1){
-                    obstacleGrid[i][j]=-1;
-                }}}
-        if(obstacleGrid[0][0]==-1) return 0;
-        else obstacleGrid[0][0]=1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(obstacleGrid[i][j]!=-1){
-                if(i==0 && j>0 && obstacleGrid[i][j-1]!=-1){
-                    obstacleGrid[i][j]=obstacleGrid[i][j-1];
-                }
-                else if(j==0 && i>0 && obstacleGrid[i-1][j]!=-1) obstacleGrid[i][j]=obstacleGrid[i-1][j];
-                else{
-                    if(j>0 && obstacleGrid[i][j-1]!=-1) obstacleGrid[i][j]=obstacleGrid[i][j-1];
-                    if(i>0 && obstacleGrid[i-1][j]!=-1) obstacleGrid[i][j]+=obstacleGrid[i-1][j];
-                }
+    public int uniquePathsWithObstacles(int[][] arr) {
+        if(arr[0][0]==1) return 0;
+        arr[0][0]=1;
+        int n=arr.length;
+        int m=arr[0].length;
+        for(int i=1;i<n;i++){
+            if(arr[i][0]==1){
+                while(i<n){
+                    arr[i++][0]=0;
                 }
             }
+            else arr[i][0]=1;
         }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++) System.out.print(obstacleGrid[i][j]+" ");
-        //     System.out.println();
-        // }
-        return obstacleGrid[n-1][m-1]!=-1?obstacleGrid[n-1][m-1]:0;   
+        for(int i=1;i<m;i++){
+            if(arr[0][i]==1){
+                while(i<m){
+                    arr[0][i++]=0;
+                }
+            }
+            else arr[0][i]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(arr[i][j]==1) arr[i][j]=0;
+                else arr[i][j]=arr[i-1][j]+arr[i][j-1];
+            }
+        }
+        return arr[n-1][m-1];
     }
 }
