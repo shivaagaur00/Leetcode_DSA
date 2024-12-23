9cc466f933330ca312ce7c4da9fc417d64927088
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2024-12-23 23:45:37 +0530

    Time: 311 ms (21.23%) | Memory: 63.2 MB (72.60%) - LeetSync

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minimumOperations(TreeNode root) {
+        int ans=0;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n=q.size();
+            List<Integer> arr=new ArrayList<>();
+            while(n-->0){
+                TreeNode temp=q.poll();
+                arr.add(temp.val);
+                if(temp.left!=null) q.add(temp.left);
+                if(temp.right!=null) q.add(temp.right);
+            }
+            System.out.println(arr);
+            ans+=(func(arr));
+            System.out.println(arr);
+        }
+    return ans;
+    }
+    // public int minNoOperations(List<Integer> list){
+    //     int cnt=0;
+    //     List<Integer> listSort=new ArrayList();
+    //     Map<Integer, Integer> map=new HashMap();
+
+    //     //Storing Hash(key, value) for list-value and list-index
+    //     for(int i=0; i<list.size(); i++){
+    //         map.put(list.get(i), i);
+    //     }
+
+    //     listSort.addAll(list);
+    //     Collections.sort(listSort); //Having sorted list into variable "listSort"
+
+    //     //Comparing "list" with "sortedList" and swapping with help of "map"
+    //     for(int i=0; i<list.size(); i++){
+    //         if(list.get(i)!=listSort.get(i)){
+    //             int idxS=i;
+    //             int idxT=map.get(listSort.get(i));
+
+    //             Collections.swap(list, idxS, idxT); //Swapping the values between idxS and idxT
+    //             map.put(list.get(idxS), idxS); //Updating "map" value for idxS
+    //             map.put(list.get(idxT), idxT); //Updating "map" value for idxT
+
+    //             cnt++;
+    //         }
+    //     }       
+    //     return cnt;
+    // }
+    public int func(List<Integer> arr){
+        int count=0;
+        List<Integer> sortedArr=new ArrayList<>(arr);
+        Collections.sort(sortedArr);
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.size();i++){
+            map.put(arr.get(i),i);
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr.get(i)!=sortedArr.get(i)){
+                int curr=i;
+                int idx=map.get(sortedArr.get(i));
+                Collections.swap(arr,curr,idx);
+                map.put(arr.get(i),i);
+                map.put(arr.get(idx),idx);
+                count++;
+            }
+        }
+        return count;
+    }
+}
