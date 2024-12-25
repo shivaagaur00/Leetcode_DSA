818bf9d70d2a63c641e08f900d4137f4939123d0
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2024-12-25 13:24:45 +0530

    Time: 8 ms (69.54%) | Memory: 45.1 MB (21.62%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,21 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return func(nums,0,0,target);
+        int offset=0;
+        for(int a:nums) offset+=a;
+        if (target>offset || target<(-offset)) return 0;
+        int[][] dp=new int[nums.length][2*offset+1];
+        for(int a[]:dp) Arrays.fill(a,-1);
+        return func(dp,nums,0,0,target,offset);
     }
-    public int func(int[] nums,int idx,int sum,int target){
+    public int func(int[][] dp,int[] nums,int idx,int sum,int target,int offset){
         if(idx==nums.length){
             if(target==sum) return 1;
             else return 0;
         }
-        int take=func(nums,idx+1,sum+nums[idx],target);
-        int left=func(nums,idx+1,sum-nums[idx],target);
+        if(dp[idx][sum+offset]!=-1) return dp[idx][sum+offset];
+        int take=func(dp,nums,idx+1,sum+nums[idx],target,offset);
+        int left=func(dp,nums,idx+1,sum-nums[idx],target,offset);
+        dp[idx][sum+offset]=take+left;
         return take+left;
     }
-    
 }
