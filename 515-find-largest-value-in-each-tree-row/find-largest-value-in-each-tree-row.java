2133bbc173917102e1a251a51ff84514959ff485
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2024-12-25 11:37:44 +0530

    Time: 2 ms (83.77%) | Memory: 45.3 MB (78.70%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -15,21 +15,21 @@
  */
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
-        if(root==null) return ans;
-        Queue<TreeNode> stack=new LinkedList<>();
-        stack.add(root);
-        while(!stack.isEmpty()){
-            int n=stack.size();
+        List<Integer> list=new ArrayList<>();
+        if(root==null) return list;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n=q.size();
             int max=Integer.MIN_VALUE;
-            for(int i=0;i<n;i++){
-                TreeNode temp=stack.poll();
+            while(n-->0){
+                TreeNode temp=q.poll();
+                if(temp.left!=null) q.add(temp.left);
+                if(temp.right!=null) q.add(temp.right);
                 max=Math.max(max,temp.val);
-                if(temp.left!=null) stack.add(temp.left);
-                if(temp.right!=null) stack.add(temp.right);
             }
-            ans.add(max);
+            list.add(max);
         }
-        return ans;
+        return list;
     }
 }
