34e658e96dacbd386d2967e07ad7ed13292aab28
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2024-12-29 01:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (12.88%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -5,18 +5,20 @@ class Solution {
         if(n==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
         int[] arr=new int[nums.length];
-        Arrays.fill(arr,-1);
         int take=func(nums,arr,0,n-2);
-        Arrays.fill(arr,-1);
+        Arrays.fill(arr,0);
         int nottake=func(nums,arr,1,n-1);
         return Math.max(take,nottake);
     }
     public int func(int[] nums,int[] arr,int i,int j){
-        if(i>j) return 0;
-        if(arr[i]!=-1) return arr[i];
-        int take=nums[i]+func(nums,arr,i+2,j);
-        int nottake=func(nums,arr,i+1,j);
-        arr[i]=Math.max(take,nottake);
-        return arr[i];
+        if((j-i)==1){
+            return Math.max(nums[i],nums[j]);
+        }
+        arr[i]=nums[i];
+        arr[i+1]=Math.max(nums[i],nums[i+1]);
+        for(int id=2;id<=j;id++){
+            arr[id]=Math.max((nums[id]+arr[id-2]),arr[id-1]);
+        }
+        return arr[j];
     }
 }
