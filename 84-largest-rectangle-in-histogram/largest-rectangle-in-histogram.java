f1d04a848c36ad11dab8d8633bb120baea2b0abd
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-01-08 20:11:29 +0530

    Time: 133 ms (5.50%) | Memory: 61.7 MB (5.11%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int largestRectangleArea(int[] arr) {
+        int max = 0;
+        List<Integer> leftarr=new ArrayList<>();
+        List<Integer> rightarr=new ArrayList<>();
+        int[] left=left(arr,leftarr);
+        int[] right=right(arr,rightarr);
+        for (int i = 0;i<arr.length;i++) {
+            max=Math.max(max,(arr[i]*(right[i]-left[i]-1)));
+        }
+        // Collections.reverse(rightarr);
+        // System.out.println(leftarr);
+        // System.out.println(rightarr);
+
+        return max;
+    }
+    public int[] left(int arr[],List<Integer> left) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()){
+                ans[i] = -1;
+                left.add(-1);
+            }
+
+            else{
+                ans[i] = stack.peek();
+                left.add(stack.peek());
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+    public int[] right(int arr[],List<Integer> left)  {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()){
+                ans[i] = arr.length;
+                left.add(arr.length);
+            }
+
+            else{
+                ans[i] = stack.peek();
+                left.add(stack.peek());
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+}
