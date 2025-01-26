edbbc6af95d7d3654d485eb8e7320d1d54e96e0e
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-01-26 13:38:51 +0530

    Time: 19 ms (8.04%) | Memory: 52.8 MB (83.04%) - LeetSync

--- 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.java
+++ 1455-filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.java
@@ -4,6 +4,7 @@ class Solution {
         for(int i=0;i<arr.length;i++) {
             if(maxPrice<arr[i][3] || maxDistance<arr[i][4] || (veganFriendly==1 && arr[i][2]!=1)){
                 arr[i][0]=0;
+                arr[i][1]=0;
             }
         }
         Arrays.sort(arr, (a, b) -> {
@@ -13,13 +14,12 @@ class Solution {
         return b[0] - a[0];
         }
     });
-
     for(int[] ar:arr){
         System.out.println(ar[0]);
         if(ar[0]>0){
             ans.add(ar[0]);
         }
-        // else break;
+        else break;
     }
     return ans;
     }
