8c8296bd49c51df53fffded13138f8426f53170a
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-01-31 09:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (72.91%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
@@ -7,7 +7,7 @@
  *     TreeNode() {}
  *     TreeNode(int val) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
++ *         this.val = val;/
  *         this.left = left;
  *         this.right = right;
  *     }
@@ -20,7 +20,7 @@ class Solution {
     }
     public TreeNode func(int[] nums,int i,int j){
         if(i>j) return null;
-        int mid=(i+j)/2;
+        int mid=i+(j-i)/2;
         TreeNode root=new TreeNode(nums[mid]);
         root.left=func(nums,i,mid-1);
         root.right=func(nums,mid+1,j);
