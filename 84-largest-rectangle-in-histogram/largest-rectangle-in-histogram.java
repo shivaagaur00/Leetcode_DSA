9ce85672a34911168b0bb67327edf1bcbbcc5784
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-02-10 17:41:56 +0530

    Time: 118 ms (12.13%) | Memory: 58.1 MB (17.45%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -3,14 +3,14 @@ class Solution {
         int max = 0;
         List<Integer> leftarr=new ArrayList<>();
         List<Integer> rightarr=new ArrayList<>();
-        int[] left=left(arr,leftarr);
-        int[] right=right(arr,rightarr);
+        int[] left=left(arr);
+        int[] right=right(arr);
         for (int i = 0;i<arr.length;i++) {
             max=Math.max(max,(arr[i]*(right[i]-left[i]-1)));
         }
         return max;
     }
-    public int[] left(int arr[],List<Integer> left) {
+    public int[] left(int arr[]) {
         Stack<Integer> stack = new Stack<>();
         int[] ans = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
@@ -19,19 +19,17 @@ class Solution {
             }
             if (stack.isEmpty()){
                 ans[i] = -1;
-                // left.add(-1);
             }
 
             else{
                 ans[i] = stack.peek();
-                // left.add(stack.peek());
             }
             stack.push(i);
         }
         return ans;
     }
 
-    public int[] right(int arr[],List<Integer> left)  {
+    public int[] right(int arr[])  {
         Stack<Integer> stack = new Stack<>();
         int[] ans = new int[arr.length];
         for (int i = arr.length - 1; i >= 0; i--) {
@@ -40,12 +38,10 @@ class Solution {
             }
             if (stack.isEmpty()){
                 ans[i] = arr.length;
-                left.add(arr.length);
             }
 
             else{
                 ans[i] = stack.peek();
-                left.add(stack.peek());
             }
             stack.push(i);
         }
