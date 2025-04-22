135d169d98feeb2a7e59ac57ca056c52f02f21c9
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-04-22 15:57:28 +0530

    Time: 4 ms (21.87%) | Memory: 46.8 MB (89.42%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,34 +1,32 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
+        List<List<Integer>> conn=new ArrayList<>();
         for(int i=0;i<n;i++){
-            arr.add(new ArrayList<>());
+            conn.add(new ArrayList<>());
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    arr.get(j).add(i);
-                    arr.get(i).add(j);
+                if(isConnected[i][j]==1){
+                    conn.get(i).add(j);
+                    conn.get(j).add(i);
                 }
             }
         }
         int ans=0;
-        int[] vis=new int[n];
+        boolean vis[]=new boolean[n];
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
+            if(!vis[i]){
+                dfs(vis,i,conn);
                 ans++;
-                dfs(vis,arr,i);
             }
         }
         return ans;
     }
-    public void dfs(int[] vis,ArrayList<ArrayList<Integer>> arr,int i){
-        vis[i]=1;
-        for(int j=0;j<arr.get(i).size();j++){
-            if(vis[arr.get(i).get(j)]==0){
-                dfs(vis,arr,arr.get(i).get(j));
-            }
+    public void dfs(boolean[] vis,int i,List<List<Integer>> conn){
+        vis[i]=true;
+        for(int j=0;j<conn.get(i).size();j++){
+            if(!vis[conn.get(i).get(j)]) dfs(vis,conn.get(i).get(j),conn);
         }
     }
 }
