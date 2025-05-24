ad90daf4f09ff4de3583650acb9d71c9cd240a87
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-05-25 00:11:09 +0530

    Time: 3 ms (94.94%) | Memory: 45.1 MB (62.68%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,40 +1,31 @@
 class Solution {
-    private int solve(int i, int j, String s1, String s2, int dp[][]) {
-        if (i == 0 && j == 0) {
-            if (s1.charAt(i) == s2.charAt(j))
-                return 0;
-            else
-                return 1;
-        }
-        if (j == -1)
-            return i + 1;
-        if (i == -1)
-            return j + 1;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int insert = 0;
-        int replace = 0;
-        int delete = 0;
-
-        if (s1.charAt(i) == s2.charAt(j)) 
-            return dp[i][j] = solve(i - 1, j - 1, s1, s2, dp);
-        else {
-            insert = 1 + solve(i, j - 1, s1, s2, dp);
-            replace = 1 + solve(i - 1, j - 1, s1, s2, dp);
-            delete = 1 + solve(i - 1, j, s1, s2, dp);
-        }
-
-        return dp[i][j] = Math.min(insert, Math.min(replace, delete));
-    }
-
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()][word2.length()];
+        int n=word1.length();
+        int m=word2.length();
+        int[][] dp=new int[n][m];
+        for(int a[]:dp){
+            Arrays.fill(a,-1);
+        }
+        return func(n-1,m-1,word1,word2,dp);
+    }
+    public int func(int i,int j,String s1,String s2,int[][] dp){
+        if(i==0 && j==0){
+            if(s1.charAt(0)==s2.charAt(0)) return 0;
+            return 1;
+        }
+        if(i==-1) return j+1;
+        if(j==-1) return i+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int in=0;
+        int re=0;
+        int de=0;
+        if(s1.charAt(i)==s2.charAt(j)) return dp[i][j]=func(i-1,j-1,s1,s2,dp);
+        else{
+            in =1+func(i-1,j,s1,s2,dp);
+            de =1+func(i,j-1,s1,s2,dp);
+            re =1+func(i-1,j-1,s1,s2,dp);
+        }
+        return dp[i][j] = Math.min(in, Math.min(re, de));
 
-        for (int[] I : dp)
-            Arrays.fill(I, -1);
-
-        return solve(word1.length() - 1, word2.length() - 1, word1, word2, dp);
     }
 }
