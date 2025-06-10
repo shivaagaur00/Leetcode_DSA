a51e8ac97c39fc7c4199d68e4424409f6b47f60e
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-10 16:01:00 +0530

    Time: 56 ms (15.33%) | Memory: 46.3 MB (5.16%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,19 +10,35 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        int n=lists.length;
         if(lists.length==0) return null;
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for(ListNode list:lists){
-            while(list!=null){
-                pq.add(list.val);
-                list=list.next;
+        ListNode ans=lists[0];
+        ListNode temp=ans;
+        for(int i=1;i<n;i++){
+            ans=mergeList(ans,lists[i]);
+        }
+        return ans;
+    }
+    public ListNode mergeList(ListNode root1,ListNode root2){
+        ListNode dummy=new ListNode(0);
+        ListNode ans=dummy;
+        while(root1!=null && root2!=null){
+            if(root1.val<root2.val){
+                ans.next=new ListNode(root1.val);
+                ans=ans.next;
+                root1=root1.next;
+            }
+            else{
+                ans.next=new ListNode(root2.val);
+                ans=ans.next;
+                root2=root2.next;
             }
         }
-        ListNode ans=new ListNode(0);
-        ListNode dummy=ans;
-        while(!pq.isEmpty()){
-            ans.next=new ListNode(pq.poll());
-            ans=ans.next;
+        if(root1!=null){
+            ans.next=root1;
+        }
+        if(root2!=null){
+            ans.next=root2;
         }
         return dummy.next;
     }
