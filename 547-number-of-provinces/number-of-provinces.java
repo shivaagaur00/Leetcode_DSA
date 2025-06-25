7837da20a4f6165dcb1e1ced18bcf63160096d30
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-06-25 17:38:11 +0530

    Time: 4 ms (18.57%) | Memory: 46.8 MB (90.09%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,32 +1,37 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        List<List<Integer>> conn=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            conn.add(new ArrayList<>());
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    conn.get(i).add(j);
-                    conn.get(j).add(i);
-                }
+       int n=isConnected.length;
+       List<List<Integer>> conn=new ArrayList<>();
+       for(int i=0;i<n;i++){
+        conn.add(new ArrayList<>());
+       } 
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(isConnected[i][j]==1){
+                conn.get(i).add(j);
+                conn.get(j).add(i);
             }
         }
-        int ans=0;
-        boolean vis[]=new boolean[n];
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(vis,i,conn);
-                ans++;
-            }
+       }
+       int ans=0;
+
+       boolean[] vis=new boolean[n];
+       Arrays.fill(vis,false);
+       for(int i=0;i<n;i++){
+        if(!vis[i]){
+            ans++;
+            dfs(conn,i,vis);
         }
-        return ans;
+       }
+       return ans;
     }
-    public void dfs(boolean[] vis,int i,List<List<Integer>> conn){
-        vis[i]=true;
-        for(int j=0;j<conn.get(i).size();j++){
-            if(!vis[conn.get(i).get(j)]) dfs(vis,conn.get(i).get(j),conn);
+    public void dfs(List<List<Integer>> conn,int id,boolean vis[]){
+        vis[id]=true;
+        for(int i=0;i<conn.get(id).size();i++){
+            if(!vis[conn.get(id).get(i)]){
+                dfs(conn,conn.get(id).get(i),vis);
+            }
         }
+        return;
     }
 }
