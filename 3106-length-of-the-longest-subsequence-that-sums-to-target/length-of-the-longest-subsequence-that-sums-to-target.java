d48de733842b6a5b86123b5f1259e73b3486f818
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-07-16 19:06:29 +0530

    Time: 260 ms (56.92%) | Memory: 55.6 MB (26.71%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.java
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.java
@@ -9,13 +9,12 @@ class Solution {
             }
         }
         for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < sum + 1; j++) {
+            for (int j = 0; j < sum + 1; j++) {
                 dp[i][j] = dp[i - 1][j];
                 if (j >= nums.get(i - 1) && dp[i - 1][j - nums.get(i - 1)] != -1)
                     dp[i][j] = Math.max(1 + dp[i - 1][j - nums.get(i - 1)], dp[i][j]);
             }
         }
-
         return dp[n][sum];
     }
 }
