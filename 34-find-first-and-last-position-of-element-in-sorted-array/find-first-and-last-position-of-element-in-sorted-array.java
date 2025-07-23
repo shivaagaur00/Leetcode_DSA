05130b874f5a405f3357fcb169af14756e6f0d0b
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-07-23 22:55:57 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (67.76%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,33 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange(int[] nums, int tar) {
         int arr[] = new int[2];
         arr[0] = -1;
         arr[1] = -1;
-        boolean flag = true;
-        int i=0,j;
-        j=nums.length-1;
+        arr[0]=binarySearch(nums,tar,true);
+        arr[1]=binarySearch(nums,tar,false);
+        return arr;
+    }
+    public int binarySearch(int[] nums,int tar,boolean flag){
+        int i=0,j=nums.length-1;
+        int ans=-1;
         while(i<=j){
-            int mid=(i+j)/2;
-            if (nums[mid]==target){
-                for(int x=mid; x>=0; x--){
-                    if(nums[x]==target) arr[0]  =x;
-                    else break;
-                }
-                for(int x=mid; x<nums.length; x++){
-                    if(nums[x]==target) arr[1]  =x;
-                    else break;
-                }
-                break;
-            }
-            else if(nums[mid]<target){
+            int mid=(j-i)/2+i;
+            if(nums[mid]<tar){
                 i=mid+1;
             }
-            else{
+            else if(nums[mid]>tar){
                 j=mid-1;
             }
+            else{
+                ans=mid;
+                if(flag){
+                    j=mid-1;
+                }
+                else{
+                    i=mid+1;
+                }
+            }
         }
-        return arr;
+        return ans;
     }
 }
