d66043a332dc7e05058ecc908089eb6f2eec261d
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-07-23 19:24:37 +0530

    Time: 2071 ms (5.48%) | Memory: 54.6 MB (6.16%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        Stack<Character> st=new Stack<>();
+        boolean flag=x>y;
+        String r="ba";
+        if(flag){
+            r="ab";
+        }
+        int ans=0;
+        for(char c:s.toCharArray()){
+            if(!st.isEmpty() && st.peek()==r.charAt(0) && c==r.charAt(1)){
+                st.pop();
+                if(flag) ans+=x;
+                else ans+=y;
+            }
+            else st.push(c);
+        }
+        s="";
+        while(!st.isEmpty()){
+            s+=(st.pop());
+        }
+        for(char c:s.toCharArray()){
+            if(!st.isEmpty() && st.peek()==r.charAt(0) && c==r.charAt(1)){
+                st.pop();
+                if(flag) ans+=y;
+                else ans+=x;
+            }
+            else st.push(c);
+        }
+        return ans;
+    }
+}
