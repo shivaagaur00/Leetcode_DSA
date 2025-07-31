fcf2c732f2c5aa2a051fde36dd2d444edcaae9a0
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-07-31 23:40:53 +0530

    Time: 2 ms (67.54%) | Memory: 42.8 MB (29.51%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,33 +1,30 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Map<Integer,Set<Integer>> map=new HashMap<>();
         int m=grid.length;
         int n=grid[0].length;
-        boolean vis[][]=new boolean[m][n];
+        int dir[][]={{0,1},{1,0},{0,-1},{-1,0}};
         Queue<pair> q=new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    q.add(new pair(i,j));
-                    vis[i][j]=true;
-                }
+                if(grid[i][j]==2) q.add(new pair(i,j));
             }
         }
         int ans=0;
-        int direction[][]={{0,1},{1,0},{-1,0},{0,-1}};
         while(!q.isEmpty()){
-            int nn=q.size();
+            int t=q.size();
             boolean isRot=false;
-            while(nn-->0){
+            // ans++;
+            while(t-->0){
                 pair temp=q.poll();
-                int currI=temp.i;
-                int currJ=temp.j;
-                for(int[] a:direction){
-                    int ci=currI+a[0];
-                    int cj=currJ+a[1];
-                    if(ci>=0 && cj>=0 && ci<m && cj<n && !vis[ci][cj] && grid[ci][cj]==1){
-                        q.add(new pair(ci,cj));
-                        vis[ci][cj]=true;
-                        grid[ci][cj]=2;
+                int ii=temp.i;
+                int jj=temp.j;
+                for(int a[]:dir){
+                    int ni=ii+a[0];
+                    int nj=jj+a[1];
+                    if(ni>=0 && nj>=0 && ni<m && nj<n && grid[ni][nj]==1){
+                        q.add(new pair(ni,nj));
+                        grid[ni][nj]=2;
                         isRot=true;
                     }
                 }
@@ -35,14 +32,12 @@ class Solution {
             if(isRot) ans++;
             else break;
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1)
-                    return -1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) return -1;
             }
         }
         return ans;
-    
     }
     class pair{
         int i;
