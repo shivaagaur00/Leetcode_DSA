74710e3ae53f3350a1c3a213032ddc43c3ba628d
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2025-08-15 22:44:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (91.17%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,24 +1,19 @@
-
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0],nums[1]);
-        int[] arr=new int[nums.length];
-        int take=func(nums,arr,0,n-2);
-        Arrays.fill(arr,0);
-        int nottake=func(nums,arr,1,n-1);
-        return Math.max(take,nottake);
+        if(nums.length==1)  return nums[0];
+        else if(nums.length==2) return Math.max(nums[0],nums[1]);
+        // int a=func(nums,0,nums.length-2);
+        // int b=func(nums,1,nums.length-1);
+        // System.out.println(a+" "+b);
+        return Math.max(func(nums,0,nums.length-2),func(nums,1,nums.length-1));
     }
-    public int func(int[] nums,int[] arr,int i,int j){
-        if((j-i)==1){
-            return Math.max(nums[i],nums[j]);
+    public int func(int nums[],int i,int n){
+        int dp[]=new int[nums.length];
+        dp[i]=nums[i];
+        dp[i+1]=Math.max(nums[i],nums[i+1]); 
+        for(int id=i+2;id<=n;id++){
+            dp[id]=Math.max(dp[id-1],nums[id]+dp[id-2]);
         }
-        arr[i]=nums[i];
-        arr[i+1]=Math.max(nums[i],nums[i+1]);
-        for(int id=2;id<=j;id++){
-            arr[id]=Math.max((nums[id]+arr[id-2]),arr[id-1]);
-        }
-        return arr[j];
+        return dp[n];
     }
 }
