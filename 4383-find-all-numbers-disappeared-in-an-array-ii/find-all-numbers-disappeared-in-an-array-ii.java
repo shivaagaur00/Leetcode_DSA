33d4472e7126ef861ee9483e441997c33fa606fe
Shiva Gaur <115822667+shivaagaur00@users.noreply.github.com>
2026-09-02 11:47:14 +0530

    Time: 151 ms (5.00%) | Memory: 193.8 MB (5.03%) - LeetSync

--- 4383-find-all-numbers-disappeared-in-an-array-ii/find-all-numbers-disappeared-in-an-array-ii.java
+++ 4383-find-all-numbers-disappeared-in-an-array-ii/find-all-numbers-disappeared-in-an-array-ii.java
@@ -2,19 +2,21 @@ class Solution {
     public List<List<Integer>> findDisappearedNumbers(int[] nums, int lower, int upper) {
         List<List<Integer>> finalans = new ArrayList<>();
         Arrays.sort(nums);
-        HashSet<Integer> set= new HashSet<>();
-        for(int a: nums) set.add(a);
-        List<Integer> ans= new ArrayList<>();
-        for(int j=lower;j<=upper;j++){
-            if(!set.contains(j)) ans.add(j);
+        HashSet<Integer> set = new HashSet<>();
+        for (int a : nums)
+            set.add(a);
+        List<Integer> ans = new ArrayList<>();
+        for (int j = lower; j <= upper; j++) {
+            if (!set.contains(j))
+                ans.add(j);
         }
-        int i=0;
-        while(i<ans.size()){
-            int start =ans.get(i);
-            int end =start;
-            while(i+1 < ans.size() && ans.get(i+1) == end+1){
+        int i = 0;
+        while (i < ans.size()) {
+            int start = ans.get(i);
+            int end = start;
+            while (i + 1 < ans.size() && ans.get(i + 1) == end + 1) {
                 i++;
-                end =ans.get(i);
+                end = ans.get(i);
             }
             List<Integer> group = new ArrayList<>();
             group.add(start);
